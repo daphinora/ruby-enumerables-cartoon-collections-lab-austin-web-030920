0598ac8e6a3e715942eafc645ad16874d6bd278a
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-02-29 21:21:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ end
 def summon_captain_plane(array)
   array = array.collect do |element| element+"!"
  end
-  array = 
+  array = array.map do |element| 
 end
 
 def long_planeteer_calls# code an argument here
