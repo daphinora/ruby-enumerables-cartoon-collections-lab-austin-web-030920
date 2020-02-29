80287ecc8abdaa24791b758cb25ed8c66bd53178
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-02-29 21:20:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_plane(array)
-  array.collect do |element| element+!
-    puts 
+  array.collect do |element| element+"!"
+  
 end
 
 def long_planeteer_calls# code an argument here
