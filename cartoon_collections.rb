f9b639a964c0e8a09729d6b855f7161c7354dcbb
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-02-29 22:13:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,7 +27,6 @@ def find_the_cheese(array)
     return true
   end
     puts array
-  cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.find { |index| array.include?(index) } 
 end
 
